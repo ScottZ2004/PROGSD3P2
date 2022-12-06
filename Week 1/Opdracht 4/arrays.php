b38f93a49a25308157cb776701036e95f3470ce1
Scott van den Broek <scottzico2004@gmail.com>
2022-12-06 15:58:34 +0100

    opdracht 2 van opdracht 4 af

--- Week 1/Opdracht 4/arrays.php
+++ Week 1/Opdracht 4/arrays.php
@@ -2,7 +2,7 @@
 
 $aanhef = array(
     "aanhef" => "Beste",
-    "persoon" => "");
+    "persoon" => "klant");
 
 $korting = array(
     "product" => "U heeft in de voorgaande bestelling voor [[product]] gekozen",
@@ -11,21 +11,10 @@ $korting = array(
 
 $ondertekening = array(
   "groet" => "Met vriendelijke groet,",
-  "naam" => "",
+  "naam" => "Scott",
   "functie" => "Directeur Webshop beheer"
 );
 
-/*
-* Opdracht 1:
-* Laat de inhoud van alle arrays zien
-* Gebruik daarvoor de functies var_dump() en print_r()
-*/
-
-/*
-* Opdracht 2:
-* Vervang de waarde van persoon, in de array aanhef naar 'klant'
-* Vervang de waarde van naam, in de array ondertekening naar 'jouw naam'
-*/
 
 /*
 * Opdracht 3:
@@ -45,5 +34,7 @@ $ondertekening = array(
 */
 
 print_r(var_dump($aanhef));
+echo "<br>";
 print_r(var_dump($korting));
+echo "<br>";
 print_r(var_dump($ondertekening));
